6b84e63f68e6ca68f08dcb47ce17b7d8a36187d0
Nagashruthi1 <162799787+Nagashruthi1@users.noreply.github.com>
2024-03-09 16:46:27 +0530

    Add files via upload

---
+++ Ticket Tracker Application/src/main/java/com/gl/Ticket Tracker Application/AssignmentSpringApplication.java
@@ -0,0 +1,28 @@
+package com.gl.AssignmentSpring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.gl.AssignmentSpring.model.Ticket;
+import com.gl.AssignmentSpring.service.TicketService;
+
+@SpringBootApplication
+public class AssignmentSpringApplication implements CommandLineRunner{
+	@Autowired
+	TicketService ticketService;
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentSpringApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		
+		  ticketService.addTicket(new Ticket(1,"ticket 1","title Description","2022-12-27"));
+		  ticketService.addTicket(new Ticket(2,"ABCD","ABCD","2022-12-27"));
+		 
+	}
+
+}
---
+++ Ticket Tracker Application/src/main/java/com/gl/Ticket Tracker Application/model/Ticket.java
@@ -0,0 +1,26 @@
+package com.gl.AssignmentSpring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Ticket {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	int id;
+	String title;
+	String description;
+	String date;
+
+	public int getId() {
+		return 0;
+	}
+}
